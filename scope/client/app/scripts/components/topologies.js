52f59a155e82854cdb5ccf7dd9f0fda65c356b60
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-05-05 18:03:32 +0200

    expand topologies for easier switch

--- scope/client/app/scripts/components/topologies.js
+++ scope/client/app/scripts/components/topologies.js
@@ -9,95 +9,45 @@ var NavItemTopology = require('./nav-item-topology.js');
 
 var Topologies = React.createClass({
 
-	getInitialState: function() {
-		return {
-			topologiesVisible: false 
-		};
-	},
-
 	onTopologyClick: function(ev) {
 		ev.preventDefault();
-		AppActions.clickTopology(ev.currentTarget.rel);
+		AppActions.clickTopology(ev.currentTarget.getAttribute('rel'));
 	},
 
-	onMouseOver: function(ev) {
-		this.handleMouseDebounced(true);
-	},
+	renderTopology: function(topology, active) {
+		var className = AppStore.isUrlForTopology(topology.url, active) ? "nav-preview nav-active" : "nav-preview",
+			topologyId = AppStore.getTopologyForUrl(topology.url),
+			title = ['Topology: ' + topology.name,
+			'Nodes: ' + topology.stats.node_count,
+			'Connections: ' + topology.stats.node_count].join('\n');
 
-	onMouseOut: function(ev) {
-		this.handleMouseDebounced(false);
-	},
-
-	componentWillMount: function() {
-		this.handleMouseDebounced = _.debounce(function(isOver) {
-			this.setState({
-				topologiesVisible: isOver 
-			});
-		}, 200);
-	},
-
-	getActiveTopology: function() {
-		var activeTopology = _.find(this.props.topologies, function(topology) {
-			return AppStore.getTopologyForUrl(topology.url) === this.props.active;
-		}, this);
-
-		if (activeTopology) {
-			var title = ['Topology: ' + activeTopology.name,
-				'Nodes: ' + activeTopology.stats.node_count,
-				'Connections: ' + activeTopology.stats.node_count].join('\n');
-
-			return (
-				<div className="nav-preview" onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
-					<div rel={this.props.active} title={title}
-						onClick={this.props.onClick}>
-						<div className="nav-topology-frame">
-							<span className="nav-topology-nodes">{activeTopology.stats.node_count}</span>
-							<span className="nav-topology-divider" />
-							<span className="nav-topology-edges">{activeTopology.stats.edge_count}</span>
-						</div>
-						<div className="nav-label">
-							{activeTopology.name}
-						</div>
+		return (
+			<div className={className} key={topologyId} rel={topologyId} onClick={this.onTopologyClick}>
+				<div title={title}>
+					<div className="nav-topology-frame">
+						<span className="nav-topology-nodes">{topology.stats.node_count}</span>
+						<span className="nav-topology-divider" />
+						<span className="nav-topology-edges">{topology.stats.edge_count}</span>
+					</div>
+					<div className="nav-label">
+						{topology.name}
 					</div>
 				</div>
-			);
-		}
-
-		return (<div />);
+			</div>
+		);
 	},
 
 	render: function() {
-		var handleClick = this.onTopologyClick,
-			onMouseOut = this.onMouseOut,
-			onMouseOver = this.onMouseOver,
-			activeTopologyId = this.props.active,
-			activeTopology = this.getActiveTopology(),
-			navClassName = "nav navbar-nav";
+		var activeTopologyId = this.props.active,
 			topologies = _.sortBy(this.props.topologies, function(topology) {
 				return topology.name;
 			});
 
 		return (
 			<div className="navbar-topology">
-				{activeTopology}
-				{this.state.topologiesVisible && <ul className={navClassName} onMouseOut={this.onMouseOut} onMouseOver={this.onMouseOver}>
-					{_.map(this.props.topologies, function(topology) {
-						var topologyId = AppStore.getTopologyForUrl(topology.url),
-							activeClass = topologyId === activeTopologyId ? 'active' : '';
-
-						return (
-							<NavItemTopology
-								key={topologyId}
-								rel={topologyId}
-								url={topology.url}
-								nodes={topology.stats.node_count}
-								edges={topology.stats.edge_count}
-								active={activeClass}
-								onClick={handleClick}
-								name={topology.name} />
-						);
-					})}
-				</ul>}
+				{topologies.map(function(topology) {
+					return this.renderTopology(topology, activeTopologyId);
+				}, this)}
 			</div>
 		);
 	}
