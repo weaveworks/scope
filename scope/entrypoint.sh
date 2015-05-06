07c622e578f05c1023e55712cdaf4a07b8da7568
Tom Wilkie <tom@weave.works>
2015-05-06 17:49:13 +0000

    Setup scope container for weave dns, if its running.

---
+++ scope/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+usage() {
+	echo "$0 --dns <IP> --hostname <NAME> --searchpath <SEARCHPATH>"
+	exit 1
+}
+
+# This script exists to modify the network settings in the scope containers
+# as docker doesn't allow it when started with --net=host
+while true; do
+    case "$1" in
+        --dns)
+            [ $# -gt 1 ] || usage
+            DNS_SERVER="$2"
+            shift 2
+            ;;
+        --hostname)
+            [ $# -gt 1 ] || usage
+            HOSTNAME="$2"
+            shift 2
+            ;;
+        --searchpath)
+            [ $# -gt 1 ] || usage
+            SEARCHPATH="$2"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ -n "$DNS_SERVER" -a -n "$SEARCHPATH" ]; then
+    echo "domain $SEARCHPATH" >/etc/resolv.conf
+    echo "search $SEARCHPATH" >>/etc/resolv.conf
+    echo "nameserver $DNS_SERVER" >>/etc/resolv.conf
+fi
+
+if [ -n "$HOSTNAME" ]; then
+    echo "$HOSTNAME" >/etc/hostname
+    hostname -F /etc/hostname
+fi
+
+/usr/bin/supervisord -c /etc/supervisord.conf
