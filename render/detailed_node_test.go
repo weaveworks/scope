7e021baf3b8685ff119331ae839b9b2b9b7a8aa3
Tom Wilkie <tom@weave.works>
2015-06-17 13:16:39 +0000

    Add test for MakeDetailedNode.

--- render/detailed_node_test.go
+++ render/detailed_node_test.go
@@ -7,10 +7,6 @@ import (
 	"github.com/weaveworks/scope/render"
 )
 
-func TestMakeDetailedNode(t *testing.T) {
-	t.Skip("TODO")
-}
-
 func TestOriginTable(t *testing.T) {
 	if _, ok := render.OriginTable(rpt, "not-found"); ok {
 		t.Errorf("unknown origin ID gave unexpected success")
@@ -57,5 +53,62 @@ func TestOriginTable(t *testing.T) {
 		if !reflect.DeepEqual(want, have) {
 			t.Errorf("%q: %s", originID, diff(want, have))
 		}
+	}
+}
+
+func TestMakeDetailedNode(t *testing.T) {
+	renderableNode := render.ContainerRenderer.Render(rpt)[serverContainerID]
+	have := render.MakeDetailedNode(rpt, renderableNode)
+	want := render.DetailedNode{
+		ID:         serverContainerID,
+		LabelMajor: "server",
+		LabelMinor: serverHostName,
+		Pseudo:     false,
+		Tables: []render.Table{
+			{
+				Title:   "Connections",
+				Numeric: true,
+				Rows: []render.Row{
+					{"Bytes ingress", "150", ""},
+					{"Bytes egress", "1500", ""},
+				},
+			},
+			{
+				Title:   "Origin Endpoint",
+				Numeric: false,
+				Rows: []render.Row{
+					{"Endpoint", "192.168.1.1", ""},
+					{"Port", "80", ""},
+				},
+			},
+			{
+				Title:   "Origin Process",
+				Numeric: false,
+				Rows: []render.Row{
+					{"Name (comm)", "apache", ""},
+					{"PID", "215", ""},
+				},
+			},
+			{
+				Title:   "Origin Container",
+				Numeric: false,
+				Rows: []render.Row{
+					{"Container ID", "5e4d3c2b1a", ""},
+					{"Container name", "server", ""},
+				},
+			},
+			{
+				Title:   "Origin Host",
+				Numeric: false,
+				Rows: []render.Row{
+					{"Host name", "server.hostname.com", ""},
+					{"Load", "0.01 0.01 0.01", ""},
+					{"Operating system", "Linux", ""},
+				},
+			},
+		},
+	}
+	if !reflect.DeepEqual(want, have) {
+		t.Errorf("%s", diff(want, have))
 	}
 }
