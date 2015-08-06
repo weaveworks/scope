3fd5fbcf259b328f70c31e72c9b0bb6dcdb8102d
Peter Bourgon <peter@bourgon.org>
2015-08-06 19:30:58 +0200

    app may only log to stderr, just like probe

--- app/main.go
+++ app/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"log/syslog"
 	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
@@ -23,7 +22,6 @@ var version = "dev"
 func main() {
 	var (
 		defaultProbes = []string{fmt.Sprintf("localhost:%d", xfer.ProbePort), fmt.Sprintf("scope.weave.local:%d", xfer.ProbePort)}
-		logfile       = flag.String("log", "stderr", "stderr, syslog, or filename")
 		batch         = flag.Duration("batch", 1*time.Second, "batch interval")
 		window        = flag.Duration("window", 15*time.Second, "window")
 		listen        = flag.String("http.address", ":"+strconv.Itoa(xfer.AppPort), "webserver listen address")
@@ -35,30 +33,6 @@ func main() {
 	if *printVersion {
 		fmt.Println(version)
 		return
-	}
-
-	switch *logfile {
-	case "stderr":
-		break // by default
-
-	case "syslog":
-		w, err := syslog.New(syslog.LOG_INFO, "scope-app")
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		defer w.Close()
-		log.SetFlags(0)
-		log.SetOutput(w)
-
-	default: // file
-		f, err := os.OpenFile(*logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		defer f.Close()
-		log.SetOutput(f)
 	}
 
 	rand.Seed(time.Now().UnixNano())
