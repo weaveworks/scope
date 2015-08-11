ff86370df8199973b6d5ee2035d3516c78990a9f
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-08-11 11:38:00 +0100

    fixing and simplifying circle build
    The integration tests were using two different versions of weave. One to
    launch the weave container, and another used by scope.

--- integration/200_clustering_2_test.sh
+++ integration/200_clustering_2_test.sh
@@ -5,6 +5,7 @@
 start_suite "Launch 2 scopes and check they cluster"
 
 weave_on $HOST2 launch
+weave_on $HOST2 launch-dns
 docker_on $HOST2 run -dit --name db1 peterbourgon/tns-db
 container_id=$(docker_on $HOST2 run -dit --name app1 --link db1:db1 peterbourgon/tns-app)
 
@@ -13,7 +14,7 @@ scope_on $HOST2 launch $HOST1
 
 SUCCESS=
 for i in {1..10}; do
-  if (curl $HOST1:4040/api/topology/containers | grep "$container_id" >/dev/null); then
+  if (curl -s $HOST1:4040/api/topology/containers | grep "$container_id" >/dev/null); then
     SUCCESS=1
     break
   fi
