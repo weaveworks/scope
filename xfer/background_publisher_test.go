8265abad8b3cd54068f35f48bb74191c6453e315
Tom Wilkie <tom@weave.works>
2015-09-25 08:56:24 +0000

    Test for BackgroundPublisher

--- xfer/background_publisher_test.go
+++ xfer/background_publisher_test.go
@@ -1,7 +1,29 @@
 package xfer_test
 
-import "testing"
+import (
+	"bytes"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/weaveworks/scope/test"
+	"github.com/weaveworks/scope/xfer"
+)
 
 func TestBackgroundPublisher(t *testing.T) {
-	t.Skip("TODO")
+	mp := mockPublisher{}
+	backgroundPublisher := xfer.NewBackgroundPublisher(&mp)
+	defer backgroundPublisher.Stop()
+	runtime.Gosched()
+
+	for i := 0; i < 10; i++ {
+		err := backgroundPublisher.Publish(&bytes.Buffer{})
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+
+		test.Poll(t, 100*time.Millisecond, i, func() interface{} {
+			return mp.count
+		})
+	}
 }
