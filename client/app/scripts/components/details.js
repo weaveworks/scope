c0e7ade9e94da04da134fb0eb5e443664f5d6593
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-11-13 15:29:23 +0100

    Add x to close details panel again
    Fixes #667

--- client/app/scripts/components/details.js
+++ client/app/scripts/components/details.js
@@ -1,8 +1,14 @@
 const React = require('react');
 
+const AppActions = require('../actions/app-actions');
 const NodeDetails = require('./node-details');
 
 const Details = React.createClass({
+
+  handleClickClose: function(ev) {
+    ev.preventDefault();
+    AppActions.clickCloseDetails();
+  },
 
   render: function() {
     return (
@@ -10,6 +16,11 @@ const Details = React.createClass({
         <div style={{height: '100%', paddingBottom: 8, borderRadius: 2,
           backgroundColor: '#fff',
           boxShadow: '0 10px 30px rgba(0, 0, 0, 0.19), 0 6px 10px rgba(0, 0, 0, 0.23)'}}>
+          <div className="details-tools-wrapper">
+            <div className="details-tools">
+              <span className="fa fa-close" onClick={this.handleClickClose} />
+            </div>
+          </div>
           <NodeDetails {...this.props} />
         </div>
       </div>
