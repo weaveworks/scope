0929e1472587f5d9794263fdc6e90f5747701ec0
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-12-07 14:39:15 +0000

    Removing dead EdgeMetadata rendering code

--- render/render.go
+++ render/render.go
@@ -7,7 +7,6 @@ import (
 // Renderer is something that can render a report to a set of RenderableNodes.
 type Renderer interface {
 	Render(report.Report) RenderableNodes
-	EdgeMetadata(rpt report.Report, localID, remoteID string) report.EdgeMetadata
 	Stats(report.Report) Stats
 }
 
@@ -38,15 +37,6 @@ func (r Reduce) Render(rpt report.Report) RenderableNodes {
 		result = result.Merge(renderer.Render(rpt))
 	}
 	return result
-}
-
-// EdgeMetadata produces an EdgeMetadata for a given edge.
-func (r Reduce) EdgeMetadata(rpt report.Report, localID, remoteID string) report.EdgeMetadata {
-	metadata := report.EdgeMetadata{}
-	for _, renderer := range r {
-		metadata = metadata.Merge(renderer.EdgeMetadata(rpt, localID, remoteID))
-	}
-	return metadata
 }
 
 // Stats implements Renderer
@@ -117,37 +107,4 @@ func (m Map) render(rpt report.Report) (RenderableNodes, map[string]report.IDLis
 	}
 
 	return output, mapped
-}
-
-// EdgeMetadata gives the metadata of an edge from the perspective of the
-// srcRenderableID. Since an edgeID can have multiple edges on the address
-// level, it uses the supplied mapping function to translate address IDs to
-// renderable node (mapped) IDs.
-func (m Map) EdgeMetadata(rpt report.Report, srcRenderableID, dstRenderableID string) report.EdgeMetadata {
-	// First we need to map the ids in this layer into the ids in the underlying layer
-	_, mapped := m.render(rpt)        // this maps from old -> new
-	inverted := map[string][]string{} // this maps from new -> old(s)
-	for k, vs := range mapped {
-		for _, v := range vs {
-			existing := inverted[v]
-			existing = append(existing, k)
-			inverted[v] = existing
-		}
-	}
-
-	// Now work out a slice of edges this edge is constructed from
-	oldEdges := []struct{ src, dst string }{}
-	for _, oldSrcID := range inverted[srcRenderableID] {
-		for _, oldDstID := range inverted[dstRenderableID] {
-			oldEdges = append(oldEdges, struct{ src, dst string }{oldSrcID, oldDstID})
-		}
-	}
-
-	// Now recurse for each old edge
-	output := report.EdgeMetadata{}
-	for _, edge := range oldEdges {
-		metadata := m.Renderer.EdgeMetadata(rpt, edge.src, edge.dst)
-		output = output.Merge(metadata)
-	}
-	return output
 }
