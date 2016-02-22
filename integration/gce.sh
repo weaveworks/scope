458c5aae3959e3e0961c9c7ad1bf22c5690fb970
Tom Wilkie <tom.wilkie@gmail.com>
2016-02-22 12:14:36 +0000

    Bump template name to get latest version of docker.

--- integration/gce.sh
+++ integration/gce.sh
@@ -5,6 +5,6 @@ set -e
 . ./config.sh
 
 export PROJECT=scope-integration-tests
-export TEMPLATE_NAME="test-template-4"
+export TEMPLATE_NAME="test-template-5"
 export NUM_HOSTS=3
 . "../tools/integration/gce.sh" "$@"
