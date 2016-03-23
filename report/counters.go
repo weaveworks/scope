30b81a581b3a7f532c4dc7b6920bfc27a1193478
Paul Bellamy <paul.a.bellamy@gmail.com>
2016-03-23 16:20:57 +0000

    adding tests for report.Counters.String

--- report/counters.go
+++ report/counters.go
@@ -88,21 +88,25 @@ func (c Counters) Merge(other Counters) Counters {
 	return Counters{output}
 }
 
-func (c Counters) String() string {
-	if c.psMap == nil {
-		return "{}"
+func (c Counters) ForEach(f func(key string, val int)) {
+	if c.psMap != nil {
+		keys := c.psMap.Keys()
+		sort.Strings(keys)
+		for _, key := range keys {
+			if val, ok := c.psMap.Lookup(key); ok {
+				f(key, val.(int))
+			}
+		}
 	}
-	keys := []string{}
-	for _, k := range c.psMap.Keys() {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
+}
 
+func (c Counters) String() string {
 	buf := bytes.NewBufferString("{")
-	for _, key := range keys {
-		val, _ := c.psMap.Lookup(key)
-		fmt.Fprintf(buf, "%s: %d, ", key, val)
-	}
+	prefix := ""
+	c.ForEach(func(k string, v int) {
+		fmt.Fprintf(buf, "%s%s: %d", prefix, k, v)
+		prefix = ", "
+	})
 	fmt.Fprintf(buf, "}")
 	return buf.String()
 }
