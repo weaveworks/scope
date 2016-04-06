4c378283cba4edd4df1100e370c68b970f57ac69
Simon Howe <footless@gmail.com>
2016-04-06 11:26:25 +0200

    Pure-mixin-ify more stuff.
    Helps a lot for dragging around the canvas.

--- client/app/scripts/charts/nodes-chart-elements.js
+++ client/app/scripts/charts/nodes-chart-elements.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import reactMixin from 'react-mixin';
 
 import NodesChartEdges from './nodes-chart-edges';
 import NodesChartNodes from './nodes-chart-nodes';
 
-export default function NodesChartElements(props) {
-  return (
-    <g className="nodes-chart-elements" transform={props.transform}>
-      <NodesChartEdges layoutEdges={props.edges} selectedNodeId={props.selectedNodeId}
-        highlightedEdgeIds={props.highlightedEdgeIds}
-        hasSelectedNode={props.hasSelectedNode}
-        layoutPrecision={props.layoutPrecision} />
-      <NodesChartNodes layoutNodes={props.nodes} selectedNodeId={props.selectedNodeId}
-        selectedMetric={props.selectedMetric}
-        highlightedNodeIds={props.highlightedNodeIds}
-        hasSelectedNode={props.hasSelectedNode}
-        adjacentNodes={props.adjacentNodes}
-        nodeScale={props.nodeScale} onNodeClick={props.onNodeClick}
-        scale={props.scale} selectedNodeScale={props.selectedNodeScale}
-        topologyId={props.topologyId} layoutPrecision={props.layoutPrecision} />
-    </g>
-  );
+export default class NodesChartElements extends React.Component {
+  render() {
+    const props = this.props;
+    return (
+      <g className="nodes-chart-elements" transform={props.transform}>
+        <NodesChartEdges layoutEdges={props.edges} selectedNodeId={props.selectedNodeId}
+          highlightedEdgeIds={props.highlightedEdgeIds}
+          hasSelectedNode={props.hasSelectedNode}
+          layoutPrecision={props.layoutPrecision} />
+        <NodesChartNodes layoutNodes={props.nodes} selectedNodeId={props.selectedNodeId}
+          selectedMetric={props.selectedMetric}
+          highlightedNodeIds={props.highlightedNodeIds}
+          hasSelectedNode={props.hasSelectedNode}
+          adjacentNodes={props.adjacentNodes}
+          nodeScale={props.nodeScale} onNodeClick={props.onNodeClick}
+          scale={props.scale} selectedNodeScale={props.selectedNodeScale}
+          topologyId={props.topologyId} layoutPrecision={props.layoutPrecision} />
+      </g>
+    );
+  }
 }
+
+reactMixin.onClass(NodesChartElements, PureRenderMixin);
--- client/app/scripts/charts/nodes-chart-nodes.js
+++ client/app/scripts/charts/nodes-chart-nodes.js
@@ -1,68 +1,76 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import reactMixin from 'react-mixin';
 
 import NodeContainer from './node-container';
 
-export default function NodesChartNodes({adjacentNodes, highlightedNodeIds,
-  layoutNodes, layoutPrecision, nodeScale, onNodeClick, scale,
-  selectedMetric, selectedNodeScale, selectedNodeId, topologyId}) {
-  const zoomScale = scale;
+export default class NodesChartNodes extends React.Component {
+  render() {
+    const {adjacentNodes, highlightedNodeIds,
+      layoutNodes, layoutPrecision, nodeScale, onNodeClick, scale,
+      selectedMetric, selectedNodeScale, selectedNodeId, topologyId} = this.props;
 
-  // highlighter functions
-  const setHighlighted = node => node.set('highlighted',
-    highlightedNodeIds.has(node.get('id')) || selectedNodeId === node.get('id'));
-  const setFocused = node => node.set('focused', selectedNodeId
-    && (selectedNodeId === node.get('id')
-    || (adjacentNodes && adjacentNodes.includes(node.get('id')))));
-  const setBlurred = node => node.set('blurred', selectedNodeId && !node.get('focused'));
+    const zoomScale = scale;
 
-  // make sure blurred nodes are in the background
-  const sortNodes = node => {
-    if (node.get('blurred')) {
-      return 0;
-    }
-    if (node.get('highlighted')) {
-      return 2;
-    }
-    return 1;
-  };
+    // highlighter functions
+    const setHighlighted = node => node.set('highlighted',
+      highlightedNodeIds.has(node.get('id')) || selectedNodeId === node.get('id'));
+    const setFocused = node => node.set('focused', selectedNodeId
+      && (selectedNodeId === node.get('id')
+      || (adjacentNodes && adjacentNodes.includes(node.get('id')))));
+    const setBlurred = node => node.set('blurred', selectedNodeId && !node.get('focused'));
 
-  // TODO: think about pulling this up into the store.
-  const metric = node => (
-    node.get('metrics') && node.get('metrics')
-      .filter(m => m.get('id') === selectedMetric)
-      .first()
-  );
+    // make sure blurred nodes are in the background
+    const sortNodes = node => {
+      if (node.get('blurred')) {
+        return 0;
+      }
+      if (node.get('highlighted')) {
+        return 2;
+      }
+      return 1;
+    };
 
-  const nodesToRender = layoutNodes.toIndexedSeq()
-    .map(setHighlighted)
-    .map(setFocused)
-    .map(setBlurred)
-    .sortBy(sortNodes);
+    // TODO: think about pulling this up into the store.
+    const metric = node => (
+      node.get('metrics') && node.get('metrics')
+        .filter(m => m.get('id') === selectedMetric)
+        .first()
+    );
 
-  return (
-    <g className="nodes-chart-nodes">
-      {nodesToRender.map(node => <NodeContainer
-        blurred={node.get('blurred')}
-        focused={node.get('focused')}
-        highlighted={node.get('highlighted')}
-        topologyId={topologyId}
-        shape={node.get('shape')}
-        stack={node.get('stack')}
-        onClick={onNodeClick}
-        key={node.get('id')}
-        id={node.get('id')}
-        label={node.get('label')}
-        pseudo={node.get('pseudo')}
-        nodeCount={node.get('nodeCount')}
-        subLabel={node.get('subLabel')}
-        metric={metric(node)}
-        rank={node.get('rank')}
-        layoutPrecision={layoutPrecision}
-        selectedNodeScale={selectedNodeScale}
-        nodeScale={nodeScale}
-        zoomScale={zoomScale}
-        dx={node.get('x')}
-        dy={node.get('y')} />)}
-    </g>
-  );
+    const nodesToRender = layoutNodes.toIndexedSeq()
+      .map(setHighlighted)
+      .map(setFocused)
+      .map(setBlurred)
+      .sortBy(sortNodes);
+
+    return (
+      <g className="nodes-chart-nodes">
+        {nodesToRender.map(node => <NodeContainer
+          blurred={node.get('blurred')}
+          focused={node.get('focused')}
+          highlighted={node.get('highlighted')}
+          topologyId={topologyId}
+          shape={node.get('shape')}
+          stack={node.get('stack')}
+          onClick={onNodeClick}
+          key={node.get('id')}
+          id={node.get('id')}
+          label={node.get('label')}
+          pseudo={node.get('pseudo')}
+          nodeCount={node.get('nodeCount')}
+          subLabel={node.get('subLabel')}
+          metric={metric(node)}
+          rank={node.get('rank')}
+          layoutPrecision={layoutPrecision}
+          selectedNodeScale={selectedNodeScale}
+          nodeScale={nodeScale}
+          zoomScale={zoomScale}
+          dx={node.get('x')}
+          dy={node.get('y')} />)}
+      </g>
+    );
+  }
 }
+
+reactMixin.onClass(NodesChartNodes, PureRenderMixin);
