c5cb903be10cd8b845401b0401b7419867be104e
Jonathan Lange <jml@mumak.net>
2016-07-08 15:40:22 +0100

    Squashed 'tools/' changes from 7a66090..9e3c4e2
    9e3c4e2 Fix lint in all the build-tools scripts
    239935c shell-lint tool
    d9ab133 Script for finding files with a given type
    1b64e46 Add Weave Cloud client
    f2e40b4 Time out commands after three minutes
    2da55ce Don't spell-check compressed files
    e9749a5 Make scheduler aware of test parallelisation
    
    git-subtree-dir: tools
    git-subtree-split: 9e3c4e2bfd8a26bed488e61a3279640aff887927

---
+++ cmd/wcloud/types.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+)
+
+// Deployment describes a deployment
+type Deployment struct {
+	ID        string    `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	ImageName string    `json:"image_name"`
+	Version   string    `json:"version"`
+	Priority  int       `json:"priority"`
+	State     string    `json:"status"`
+	LogKey    string    `json:"-"`
+}
+
+// Config for the deployment system for a user.
+type Config struct {
+	RepoURL        string `json:"repo_url" yaml:"repo_url"`
+	RepoPath       string `json:"repo_path" yaml:"repo_path"`
+	RepoKey        string `json:"repo_key" yaml:"repo_key"`
+	KubeconfigPath string `json:"kubeconfig_path" yaml:"kubeconfig_path"`
+}
--- integration/gce.sh
+++ integration/gce.sh
@@ -7,15 +7,15 @@
 
 set -e
 
-: ${KEY_FILE:=/tmp/gce_private_key.json}
-: ${SSH_KEY_FILE:=$HOME/.ssh/gce_ssh_key}
-: ${IMAGE:=ubuntu-14-04}
-: ${ZONE:=us-central1-a}
-: ${PROJECT:=}
-: ${TEMPLATE_NAME:=}
-: ${NUM_HOSTS:=}
+: "${KEY_FILE:=/tmp/gce_private_key.json}"
+: "${SSH_KEY_FILE:=$HOME/.ssh/gce_ssh_key}"
+: "${IMAGE:=ubuntu-14-04}"
+: "${ZONE:=us-central1-a}"
+: "${PROJECT:=}"
+: "${TEMPLATE_NAME:=}"
+: "${NUM_HOSTS:=}"
 
-if [ -z "${PROJECT}" -o -z "${NUM_HOSTS}" -o -z "${TEMPLATE_NAME}" ]; then
+if [ -z "${PROJECT}" ] || [ -z "${NUM_HOSTS}" ] || [ -z "${TEMPLATE_NAME}" ]; then
 	echo "Must specify PROJECT, NUM_HOSTS and TEMPLATE_NAME"
 	exit 1
 fi
@@ -26,12 +26,12 @@ if [ -n "$CIRCLECI" ]; then
 fi
 
 # Setup authentication
-gcloud auth activate-service-account --key-file $KEY_FILE 1>/dev/null
-gcloud config set project $PROJECT
+gcloud auth activate-service-account --key-file "$KEY_FILE" 1>/dev/null
+gcloud config set project "$PROJECT"
 
 function vm_names {
 	local names=
-	for i in $(seq 1 $NUM_HOSTS); do
+	for i in $(seq 1 "$NUM_HOSTS"); do
 		names="host$i$SUFFIX $names"
 	done
 	echo "$names"
@@ -40,7 +40,7 @@ function vm_names {
 # Delete all vms in this account
 function destroy {
 	names="$(vm_names)"
-	if [ $(gcloud compute instances list --zone $ZONE -q $names | wc -l) -le 1 ] ; then
+	if [ "$(gcloud compute instances list --zone "$ZONE" -q "$names" | wc -l)" -le 1 ] ; then
 		return 0
 	fi
 	for i in {0..10}; do
@@ -60,23 +60,23 @@ function destroy {
 }
 
 function internal_ip {
-	jq -r ".[] | select(.name == \"$2\") | .networkInterfaces[0].networkIP" $1
+	jq -r ".[] | select(.name == \"$2\") | .networkInterfaces[0].networkIP" "$1"
 }
 
 function external_ip {
-	jq -r ".[] | select(.name == \"$2\") | .networkInterfaces[0].accessConfigs[0].natIP" $1
+	jq -r ".[] | select(.name == \"$2\") | .networkInterfaces[0].accessConfigs[0].natIP" "$1"
 }
 
 function try_connect {
 	for i in {0..10}; do
-		ssh -t $1 true && return
+		ssh -t "$1" true && return
 		sleep 2
 	done
 }
 
 function install_docker_on {
 	name=$1
-	ssh -t $name sudo bash -x -s <<EOF
+	ssh -t "$name" sudo bash -x -s <<EOF
 curl -sSL https://get.docker.com/gpg | sudo apt-key add -
 curl -sSL https://get.docker.com/ | sh
 apt-get update -qq;
@@ -87,13 +87,13 @@ service docker restart
 EOF
 	# It seems we need a short delay for docker to start up, so I put this in
 	# a separate ssh connection.  This installs nsenter.
-	ssh -t $name sudo docker run --rm -v /usr/local/bin:/target jpetazzo/nsenter
+	ssh -t "$name" sudo docker run --rm -v /usr/local/bin:/target jpetazzo/nsenter
 }
 
 function copy_hosts {
 	hostname=$1
 	hosts=$2
-	cat $hosts | ssh -t "$hostname" "sudo -- sh -c \"cat >>/etc/hosts\""
+	ssh -t "$hostname" "sudo -- sh -c \"cat >>/etc/hosts\"" < "$hosts"
 }
 
 # Create new set of VMs
@@ -101,16 +101,16 @@ function setup {
 	destroy
 
 	names="$(vm_names)"
-	gcloud compute instances create $names --image $TEMPLATE_NAME --zone $ZONE
-	gcloud compute config-ssh --ssh-key-file $SSH_KEY_FILE
+	gcloud compute instances create "$names" --image "$TEMPLATE_NAME" --zone "$ZONE"
+	gcloud compute config-ssh --ssh-key-file "$SSH_KEY_FILE"
 	sed -i '/UserKnownHostsFile=\/dev\/null/d' ~/.ssh/config
 
 	# build an /etc/hosts file for these vms
 	hosts=$(mktemp hosts.XXXXXXXXXX)
 	json=$(mktemp json.XXXXXXXXXX)
-	gcloud compute instances list --format=json >$json
+	gcloud compute instances list --format=json > "$json"
 	for name in $names; do
-		echo "$(internal_ip $json $name) $name.$ZONE.$PROJECT" >>$hosts
+		echo "$(internal_ip "$json" "$name") $name.$ZONE.$PROJECT" >> "$hosts"
 	done
 
 	for name in $names; do
@@ -118,41 +118,41 @@ function setup {
 
 		# Add the remote ip to the local /etc/hosts
 		sudo sed -i "/$hostname/d" /etc/hosts
-		sudo sh -c "echo \"$(external_ip $json $name) $hostname\" >>/etc/hosts"
-		try_connect $hostname
+		sudo sh -c "echo \"$(external_ip "$json" "$name") $hostname\" >>/etc/hosts"
+		try_connect "$hostname"
 
-		copy_hosts $hostname $hosts &
+		copy_hosts "$hostname" "$hosts" &
 	done
 
 	wait
 
-	rm $hosts $json
+	rm "$hosts" "$json"
 }
 
 function make_template {
-	gcloud compute instances create $TEMPLATE_NAME --image $IMAGE --zone $ZONE
-	gcloud compute config-ssh --ssh-key-file $SSH_KEY_FILE
+	gcloud compute instances create "$TEMPLATE_NAME" --image "$IMAGE" --zone "$ZONE"
+	gcloud compute config-ssh --ssh-key-file "$SSH_KEY_FILE"
 	name="$TEMPLATE_NAME.$ZONE.$PROJECT"
-	try_connect $name
-	install_docker_on $name
-	gcloud -q compute instances delete $TEMPLATE_NAME --keep-disks boot --zone $ZONE
-	gcloud compute images create $TEMPLATE_NAME --source-disk $TEMPLATE_NAME --source-disk-zone $ZONE
+	try_connect "$name"
+	install_docker_on "$name"
+	gcloud -q compute instances delete "$TEMPLATE_NAME" --keep-disks boot --zone "$ZONE"
+	gcloud compute images create "$TEMPLATE_NAME" --source-disk "$TEMPLATE_NAME" --source-disk-zone "$ZONE"
 }
 
 function hosts {
 	hosts=
 	args=
 	json=$(mktemp json.XXXXXXXXXX)
-	gcloud compute instances list --format=json >$json
+	gcloud compute instances list --format=json > "$json"
 	for name in $(vm_names); do
 		hostname="$name.$ZONE.$PROJECT"
 		hosts="$hostname $hosts"
-		args="--add-host=$hostname:$(internal_ip $json $name) $args"
+		args="--add-host=$hostname:$(internal_ip "$json" "$name") $args"
 	done
 	echo export SSH=\"ssh -l vagrant\"
-	echo export HOSTS=\"$hosts\"
-	echo export ADD_HOST_ARGS=\"$args\"
-	rm $json
+	echo "export HOSTS=\"$hosts\""
+	echo "export ADD_HOST_ARGS=\"$args\""
+	rm "$json"
 }
 
 case "$1" in
@@ -170,7 +170,7 @@ destroy)
 
 make_template)
 	# see if template exists
-	if ! gcloud compute images list | grep $PROJECT | grep $TEMPLATE_NAME; then
+	if ! gcloud compute images list | grep "$PROJECT" | grep "$TEMPLATE_NAME"; then
 		make_template
 	fi
 esac
