weaveworks/scope

commit b78c503004315b46da16c684159ee3231b31ecf9

Review feedback

probe/docker/container.go
@@ -386,7 +386,6 @@ func (c *container) env() map[string]string {
 func (c *container) getBaseNode() report.Node {
 	result := report.MakeNodeWith(report.MakeContainerNodeID(c.ID()), map[string]string{
 		ContainerID:       c.ID(),
-		ContainerName:     strings.TrimPrefix(c.container.Name, "/"),
 		ContainerCreated:  c.container.Created.Format(time.RFC822),
 		ContainerCommand:  c.container.Path + " " + strings.Join(c.container.Args, " "),
 		ImageID:           c.Image(),
@@ -416,6 +415,7 @@ func (c *container) GetNode() report.Node {
 		if c.container.HostConfig != nil {
 			networkMode = c.container.HostConfig.NetworkMode
 		}
+		latest[ContainerName] = strings.TrimPrefix(c.container.Name, "/")
 		latest[ContainerUptime] = uptime.String()
 		latest[ContainerRestartCount] = strconv.Itoa(c.container.RestartCount)
 		latest[ContainerNetworkMode] = networkMode

commit b3874e6e6f90e1af13f8b158375398f0227f2fb9

show names of stopped containers
This got broken in b78c503

Fixes #1691

probe/docker/container.go
@@ -439,6 +439,7 @@ func (c *container) GetNode() report.Node {
 	c.RLock()
 	defer c.RUnlock()
 	latest := map[string]string{
+		ContainerName:       strings.TrimPrefix(c.container.Name, "/"),
 		ContainerState:      c.StateString(),
 		ContainerStateHuman: c.State(),
 	}
@@ -452,7 +453,6 @@ func (c *container) GetNode() report.Node {
 		if c.container.HostConfig != nil {
 			networkMode = c.container.HostConfig.NetworkMode
 		}
-		latest[ContainerName] = strings.TrimPrefix(c.container.Name, "/")
 		latest[ContainerUptime] = uptime.String()
 		latest[ContainerRestartCount] = strconv.Itoa(c.container.RestartCount)
 		latest[ContainerNetworkMode] = networkMode