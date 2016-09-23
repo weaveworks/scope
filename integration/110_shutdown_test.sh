7fddaa9fd12bb657b745ea465f4201d280adaf00
Alfonso Acosta <fons@weave.works>
2016-09-23 15:13:25 +0000

    Print logs to debug shutdown integration test

--- integration/110_shutdown_test.sh
+++ integration/110_shutdown_test.sh
@@ -9,8 +9,10 @@ scope_on $HOST1 stop
 
 sleep 10
 
-assert_raises "docker_on $HOST1 logs weavescope 2>&1 | grep 'app exiting'"
-assert_raises "docker_on $HOST1 logs weavescope 2>&1 | grep 'probe exiting'"
+# Save stdout for debugging output
+exec 3>&1 
+assert_raises "docker_on $HOST1 logs weavescope 2>&1 | grep 'app exiting' || (docker_on $HOST1 logs weavescope 2>&3 ; false)"
+assert_raises "docker_on $HOST1 logs weavescope 2>&1 | grep 'probe exiting' || (docker_on $HOST1 logs weavescope 2>&3 ; false)"
 assert_raises "docker_on $HOST1 inspect --format='{{.State.Running}}' weavescope" "false"
 
 scope_end_suite
