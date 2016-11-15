9f8003b74bb02d9518990f1cb4153d16779971bc
fbarl <filip@hstry.co>
2016-11-15 10:13:08 +0100

    Wrote the test for iptoPaddedString.

--- client/app/scripts/utils/__tests__/string-utils-test.js
+++ client/app/scripts/utils/__tests__/string-utils-test.js
@@ -20,4 +20,13 @@ describe('StringUtils', () => {
       expect(fun(['prefix', 'suffix'])).toBe('');
     });
   });
+
+  describe('iPtoPaddedString', () => {
+    const fun = StringUtils.iPtoPaddedString;
+
+    it('it should return the formatted IP', () => {
+      expect(fun('10.244.253.4')).toBe('010.244.253.004');
+      expect(fun('0.24.3.4')).toBe('000.024.003.004');
+    });
+  });
 });
