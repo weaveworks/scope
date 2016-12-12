626e8a3e9fdf4a736ca48793caf41a0b772cbbd1
Jonathan Lange <jml@mumak.net>
2016-12-12 17:40:15 +0000

    Neater way of doing it

--- prog/app.go
+++ prog/app.go
@@ -270,11 +270,10 @@ func appMain(flags appFlags) {
 
 	handler := router(collector, controlRouter, pipeRouter, flags.externalUI)
 	if flags.logHTTP {
-		if flags.logHTTPHeaders {
-			handler = middleware.Log{LogRequestHeaders: true}.Wrap(handler)
-		} else {
-			handler = middleware.LogFailed.Wrap(handler)
-		}
+		handler = middleware.Log{
+			LogRequestHeaders: flags.logHTTPHeaders,
+			LogSuccess:        false,
+		}.Wrap(handler)
 	}
 
 	server := &graceful.Server{
