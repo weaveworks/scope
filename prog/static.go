ab28a0bfac85d79efaaf69cb7a4b33d6fee8181d
Alfonso Acosta <fons@weave.works>
2016-12-22 10:06:56 +0000

    Fix linter errors in ignored file

--- prog/static.go
+++ prog/static.go
@@ -7,8 +7,9 @@ import (
 	"github.com/weaveworks/scope/prog/staticui"
 )
 
-func GetFS(use_external bool) http.FileSystem {
-	if use_external {
+// GetFS obtains the UI code
+func GetFS(useExternal bool) http.FileSystem {
+	if useExternal {
 		return externalui.FS(false)
 	} else {
 		return staticui.FS(false)
