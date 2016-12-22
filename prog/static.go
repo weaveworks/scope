a2d09b4c233705be7dcc0ad09a58376216a81a0d
Alfonso Acosta <fons@weave.works>
2016-12-22 10:04:35 +0000

    Run gofmt (file was ignored by linter)

--- prog/static.go
+++ prog/static.go
@@ -3,8 +3,8 @@ package main
 import (
 	"net/http"
 
-	"github.com/weaveworks/scope/prog/staticui"
 	"github.com/weaveworks/scope/prog/externalui"
+	"github.com/weaveworks/scope/prog/staticui"
 )
 
 func GetFS(use_external bool) http.FileSystem {
