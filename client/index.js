c1303e181e43bd57d48da137cd031bb9514fb000
jpellizzari <jordan@weave.works>
2017-01-19 16:56:18 -0800

    Added ActionTypes and styles exports

--- client/index.js
+++ client/index.js
@@ -1,2 +1,3 @@
 exports.reducer = require('./dist/reducers/root').default;
 exports.Scope = require('./dist/components/app').default;
+exports.ActionTypes = require('./dist/constants/action-types').default;
