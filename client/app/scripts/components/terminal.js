f65f39b1285d8ed8d64e470f08650d5f5373581a
Jordan Pellizzari <jpellizzari21@gmail.com>
2017-01-31 16:09:10 -0800

    Refactored API URL generation to function calls and added tests

--- client/app/scripts/components/terminal.js
+++ client/app/scripts/components/terminal.js
@@ -9,7 +9,7 @@ import Term from 'xterm';
 import { clickCloseTerminal } from '../actions/app-actions';
 import { getNeutralColor } from '../utils/color-utils';
 import { setDocumentTitle } from '../utils/title-utils';
-import { getPipeStatus, doResizeTty, wsUrl } from '../utils/web-api-utils';
+import { getPipeStatus, doResizeTty, getWebsocketUrl } from '../utils/web-api-utils';
 
 const log = debug('scope:terminal');
 
@@ -101,14 +101,14 @@ class Terminal extends React.Component {
   }
 
   createWebsocket(term) {
-    const socket = new WebSocket(`${wsUrl}/api/pipe/${this.getPipeId()}`);
+    const socket = new WebSocket(`${getWebsocketUrl()}/api/pipe/${this.getPipeId()}`);
     socket.binaryType = 'arraybuffer';
 
     getPipeStatus(this.getPipeId(), this.props.dispatch);
 
     socket.onopen = () => {
       clearTimeout(this.reconnectTimeout);
-      log('socket open to', wsUrl);
+      log('socket open to', getWebsocketUrl());
       this.setState({connected: true});
     };
 
