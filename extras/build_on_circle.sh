340438b3c386a252a00cc6897f4d06462d1726a3
Alessandro Puccetti <alessandro@kinvolk.io>
2017-02-20 19:51:47 +0100

    extras/build_on_circle.sh: fix find error
    The find command fails on circleci running ubuntu 14.04 with the
    following message:
    
    Unix filenames usually don't contain slashes (though pathnames do).
    That means that '-name `./*.go'' will probably evaluate to false all the
    time on this system.  You might find the '-wholename' test more useful,
    or perhaps '-samefile'.  Alternatively, if you are using GNU grep, you
    could use 'find ... -print0 | grep -FzZ `./*.go''.'
    
    This fix is compatible with ubuntu 12.04
    
    This regression is caused by
    https://github.com/weaveworks/scope/pull/2068/files#diff-3072c24bd91fea6eb71aecc590951d7e

--- extras/build_on_circle.sh
+++ extras/build_on_circle.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-./in_parallel.sh "make RM=" "$(find . -maxdepth 2 -name "./*.go" -printf "%h\n" | sort -u | sed -n 's/\.\/\(.*\)/\1\/\1/p')"
+./in_parallel.sh "make RM=" "$(find . -maxdepth 2 -name "*.go" -printf "%h\n" | sort -u | sed -n 's/\.\/\(.*\)/\1\/\1/p')"
