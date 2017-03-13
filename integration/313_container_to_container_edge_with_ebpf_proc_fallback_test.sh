21047dc77f504f54d5a7def44c1c37c3a7711daa
Michael Schubert <michael@kinvolk.io>
2017-03-13 11:18:41 +0100

    Add 313_container_to_container_edge_with_ebpf_proc_fallback_test.sh

---
+++ integration/313_container_to_container_edge_with_ebpf_proc_fallback_test.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# shellcheck disable=SC1091
+. ./config.sh
+
+start_suite "Test short lived connections between containers, with ebpf proc fallback"
+
+weave_on "$HOST1" launch
+# Manually start scope in order to set
+#    `WEAVESCOPE_DOCKER_ARGS="-v /tmp:/sys/kernel/debug/tracing:ro"`
+# to make ebpf fail and test the proc fallback.
+DOCKER_HOST=tcp://${HOST1}:${DOCKER_PORT} CHECKPOINT_DISABLE=true \
+    WEAVESCOPE_DOCKER_ARGS="-v /tmp:/sys/kernel/debug/tracing:ro" \
+    "${SCOPE}" launch --probe.ebpf.connections=true
+weave_on "$HOST1" run -d --name nginx nginx
+weave_on "$HOST1" run -d --name client alpine /bin/sh -c "while true; do \
+	wget http://nginx.weave.local:80/ -O - >/dev/null || true; \
+	sleep 1; \
+done"
+
+wait_for_containers "$HOST1" 60 nginx client
+
+has_container "$HOST1" nginx
+has_container "$HOST1" client
+has_connection containers "$HOST1" client nginx
+
+# Save stdout for debugging output
+exec 3>&1
+assert_raises "docker_on $HOST1 logs weavescope 2>&1 | grep 'Error setting up the eBPF tracker, falling back to proc scanning' || (docker_on $HOST1 logs weavescope 2>&3 ; false)"
+
+scope_end_suite
