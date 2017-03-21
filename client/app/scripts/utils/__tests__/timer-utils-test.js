9dfcd213c32470b7e465a8bf60b4dd631589e5cd
jpellizzari <jordan@weave.works>
2017-03-21 12:26:52 -0700

    Replaced timely dependency

---
+++ client/app/scripts/utils/__tests__/timer-utils-test.js
@@ -0,0 +1,18 @@
+import expect from 'expect';
+import timer from '../timer-utils';
+
+describe('timer', () => {
+  it('records how long a function takes to execute', () => {
+    const add100k = (number) => {
+      for (let i = 0; i < 100000; i += 1) {
+        number += 1;
+      }
+      return number;
+    };
+
+    const timedFn = timer(add100k);
+    const result = timedFn(70);
+    expect(result).toEqual(100070);
+    expect(timedFn.time).toBeA('number');
+  });
+});
