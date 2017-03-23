d1467b67a49af1041a2ddd35a6a03f808a93b45b
Alban Crequy <alban@kinvolk.io>
2017-03-23 12:13:43 +0100

    integration test: disable flaky test 311
    Temporarily disable the test. See https://github.com/weaveworks/scope/issues/2379

--- integration/311_container_to_container_edge_with_ebpf_test.sh
+++ integration/311_container_to_container_edge_with_ebpf_test.sh
@@ -3,26 +3,30 @@
 # shellcheck disable=SC1091
 . ./config.sh
 
-start_suite "Test short lived connections between containers, with ebpf connection tracking enabled"
+start_suite "Test short lived connections between containers, with ebpf connection tracking enabled [DISABLED]"
 
 weave_on "$HOST1" launch
 scope_on "$HOST1" launch --probe.ebpf.connections=true
-weave_on "$HOST1" run -d --name nginx nginx
-weave_on "$HOST1" run -d --name client alpine /bin/sh -c "while true; do \
-	wget http://nginx.weave.local:80/ -O - >/dev/null || true; \
-	sleep 1; \
-done"
 
-wait_for_containers "$HOST1" 60 nginx client
+## Test disabled: it is currently flaky
+## https://github.com/weaveworks/scope/issues/2379
 
-has_container "$HOST1" nginx
-has_container "$HOST1" client
-
-list_containers "$HOST1"
-list_connections "$HOST1"
-
-has_connection containers "$HOST1" client nginx
-
-endpoints_have_ebpf "$HOST1"
+# weave_on "$HOST1" run -d --name nginx nginx
+# weave_on "$HOST1" run -d --name client alpine /bin/sh -c "while true; do \
+# 	wget http://nginx.weave.local:80/ -O - >/dev/null || true; \
+# 	sleep 1; \
+# done"
+#
+# wait_for_containers "$HOST1" 60 nginx client
+#
+# has_container "$HOST1" nginx
+# has_container "$HOST1" client
+#
+# list_containers "$HOST1"
+# list_connections "$HOST1"
+#
+# has_connection containers "$HOST1" client nginx
+#
+# endpoints_have_ebpf "$HOST1"
 
 scope_end_suite
