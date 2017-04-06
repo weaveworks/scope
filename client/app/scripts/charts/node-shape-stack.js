4258fbf44cc774185f619e032f87e211e2afd8a7
Filip Barl <filip.barl@gmail.com>
2017-04-06 16:37:19 +0200

    Render highlight with strokes & use non-scaled highlight for stacks.

--- client/app/scripts/charts/node-shape-stack.js
+++ client/app/scripts/charts/node-shape-stack.js
@@ -2,27 +2,17 @@ import React from 'react';
 
 import { NODE_BASE_SIZE } from '../constants/styles';
 
-
 export default function NodeShapeStack(props) {
-  const shift = props.contrastMode ? 0.15 : 0.1;
-  const highlightScale = [1, 1 + shift];
-  const dy = NODE_BASE_SIZE * shift;
-
+  const dx = NODE_BASE_SIZE * (props.contrastMode ? 0.06 : 0.05);
+  const dy = NODE_BASE_SIZE * (props.contrastMode ? 0.12 : 0.1);
+  const translateAlongAxis = t => `translate(${t * dx}, ${t * dy})`;
   const Shape = props.shape;
+
   return (
-    <g transform={`translate(0, ${dy * -2.5})`} className="stack">
-      <g transform={`scale(${highlightScale}) translate(0, ${dy})`} className="highlight">
-        <Shape {...props} />
-      </g>
-      <g transform={`translate(0, ${dy * 2})`}>
-        <Shape {...props} />
-      </g>
-      <g transform={`translate(0, ${dy * 1})`}>
-        <Shape {...props} />
-      </g>
-      <g className="only-metrics">
-        <Shape {...props} />
-      </g>
+    <g transform={translateAlongAxis(-2.5)} className="stack">
+      <g transform={translateAlongAxis(2)}><Shape {...props} /></g>
+      <g transform={translateAlongAxis(1)}><Shape {...props} highlighted={false} /></g>
+      <g transform={translateAlongAxis(0)}><Shape {...props} highlighted={false} /></g>
     </g>
   );
 }
