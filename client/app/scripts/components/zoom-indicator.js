b64305a3e4030236c5b77997d2838bc63e80cda0
Filip Barl <filip.barl@gmail.com>
2017-05-09 17:47:14 +0200

    Made the control vertical and added the buttons.

--- client/app/scripts/components/zoom-indicator.js
+++ client/app/scripts/components/zoom-indicator.js
@@ -3,26 +3,41 @@ import Slider from 'rc-slider';
 
 
 export default class ZoomIndicator extends React.Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleZoomOut = this.handleZoomOut.bind(this);
+    this.handleZoomIn = this.handleZoomIn.bind(this);
   }
 
   handleChange(value) {
-    const scale = Math.exp(value) * this.props.minScale;
-    this.props.slideAction(scale);
+    this.props.slideAction(Math.exp(value) * this.props.minScale);
+  }
+
+  handleZoomOut() {
+    const newValue = Math.max(0, this.value - (0.05 * this.max));
+    this.props.slideAction(Math.exp(newValue) * this.props.minScale);
+  }
+
+  handleZoomIn() {
+    const newValue = Math.min(this.max, this.value + (0.05 * this.max));
+    this.props.slideAction(Math.exp(newValue) * this.props.minScale);
   }
 
   render() {
     const { minScale, maxScale, scale } = this.props;
     const max = Math.log(maxScale / minScale);
     const value = Math.log(scale / minScale);
+    this.value = value;
+    this.max = max;
 
     return (
       <div className="zoom-indicator-wrapper">
         <span className="zoom-indicator">
-          Zoom <Slider min={0} max={max} value={value} step={0.001} onChange={this.handleChange} />
+          <a className="zoom-in" onClick={this.handleZoomIn}><span className="fa fa-plus" /></a>
+          <Slider max={max} value={value} vertical step={0.001} onChange={this.handleChange} />
+          <a className="zoom-out" onClick={this.handleZoomOut}><span className="fa fa-minus" /></a>
         </span>
       </div>
     );
