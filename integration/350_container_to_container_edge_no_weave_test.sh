74910317695e2dda6f8ae25b2858e164a33f621c
Alfonso Acosta <fons@weave.works>
2017-05-18 19:57:44 +0000

    Add integration test

---
+++ integration/350_container_to_container_edge_no_weave_test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# shellcheck disable=SC1091
+. ./config.sh
+
+set -x
+
+start_suite "Test short lived connections between containers without Weave (no NAT)"
+
+scope_on "$HOST1" launch
+docker_on "$HOST1" run -d --name nginx nginx
+wait_for_containers "$HOST1" 60 nginx
+nginx_ip="$(docker_on "$HOST1" inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx)"
+docker_on "$HOST1" run -d --name client alpine /bin/sh -c "while true; do \
+	wget $nginx_ip:80/ -O - >/dev/null || true; \
+	sleep 1; \
+done"
+wait_for_containers "$HOST1" 60 client
+
+has_container "$HOST1" nginx
+has_container "$HOST1" client
+has_connection containers "$HOST1" client nginx
+
+scope_end_suite
