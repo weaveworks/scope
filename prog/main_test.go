c2b9cadad9b65f9da42c78264215191ad167a033
Roland Schilter <roli@weave.works>
2017-06-06 16:24:12 +0100

    Fix assertions

--- prog/main_test.go
+++ prog/main_test.go
@@ -14,17 +14,17 @@ func TestMakeContainerFiltersFromFlags(t *testing.T) {
 	containerLabelFlags.Set(`ti\:tle2:lab\:el=2`)
 	containerLabelFlags.Set(`ti tile3:label=3`)
 
-	err := containerLabelFlags.Set(`just a string`)
+	err := containerLabelFlags.Set("just a string")
 	assert.NotNil(t, err, "Invalid container label flag not detected")
 
 	apiTopologyOptions := containerLabelFlags.apiTopologyOptions
 	assert.Equal(t, 3, len(apiTopologyOptions))
-	assert.Equal(t, `title1`, apiTopologyOptions[0].Value)
-	assert.Equal(t, `label=1`, apiTopologyOptions[0].Label)
-	assert.Equal(t, `ti:tle2`, apiTopologyOptions[1].Value)
-	assert.Equal(t, `lab:el=2`, apiTopologyOptions[1].Label)
-	assert.Equal(t, `ti tle3`, apiTopologyOptions[2].Value)
-	assert.Equal(t, `label=3`, apiTopologyOptions[2].Label)
+	assert.Equal(t, "0", apiTopologyOptions[0].Value)
+	assert.Equal(t, "title1", apiTopologyOptions[0].Label)
+	assert.Equal(t, "1", apiTopologyOptions[1].Value)
+	assert.Equal(t, "ti:tle2", apiTopologyOptions[1].Label)
+	assert.Equal(t, "2", apiTopologyOptions[2].Value)
+	assert.Equal(t, "ti tile3", apiTopologyOptions[2].Label)
 }
 
 func TestLogCensoredArgs(t *testing.T) {
