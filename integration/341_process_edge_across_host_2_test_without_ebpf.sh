ffcac6b74a6862bfbc26d8a58c80324deb3a47ff
Alfonso Acosta <fons@weave.works>
2017-07-04 17:09:22 +0000

    Add non-ebpf variants of the tests

--- integration/341_process_edge_across_host_2_test_without_ebpf.sh
+++ integration/341_process_edge_across_host_2_test_without_ebpf.sh
@@ -3,13 +3,13 @@
 # shellcheck disable=SC1091
 . ./config.sh
 
-start_suite "Test long connections (eBPF) between processes on different hosts"
+start_suite "Test long connections (procspy) between processes on different hosts"
 
 weave_on "$HOST1" launch "$HOST1" "$HOST2"
 weave_on "$HOST2" launch "$HOST1" "$HOST2"
 
-scope_on "$HOST1" launch
-scope_on "$HOST2" launch
+scope_on "$HOST1" launch --probe.ebpf.connections=false --probe.conntrack=false
+scope_on "$HOST2" launch --probe.ebpf.connections=false --probe.conntrack=false
 
 server_on "$HOST1"
 weave_proxy_on "$HOST2" run -dti --name client alpine /bin/sh -c "while true; do \
@@ -20,7 +20,6 @@ done"
 check() {
     wait_for processes "$1" 60 "nginx: worker process" nc
     has_connection processes "$1" nc "nginx: worker process"
-    endpoints_have_ebpf "$1"
     # Print connections and report in case of test failure
     list_connections "$1" processes
     echo "Report: $(curl -s "http://${1}:4040/api/report")"
