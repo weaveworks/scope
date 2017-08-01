f9e9a8ae4d6f0527e594805af6de1f7223878535
Filip Barl <filip.barl@gmail.com>
2017-08-01 14:17:03 +0200

    Shrink timeline height and make years fade out (#2778)
    * Shrink timeline height to 55px and make years fade out as hours fade in.
    
    * Address the comments.

--- client/app/scripts/utils/math-utils.js
+++ client/app/scripts/utils/math-utils.js
@@ -39,3 +39,8 @@ export function minEuclideanDistanceBetweenPoints(points) {
   });
   return minDistance;
 }
+
+// A linear mapping [a, b] -> [0, 1] (maps value x=a into 0 and x=b into 1).
+export function linearGradientValue(x, [a, b]) {
+  return (x - a) / (b - a);
+}
