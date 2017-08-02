ded2e94a4f4db6f374e7cb3916f15e36caf8680f
Filip Barl <filip.barl@gmail.com>
2017-08-02 18:43:19 +0200

    Balanced timeline zooming sensitivity between Firefox and Chrome.

--- client/app/scripts/utils/zoom-utils.js
+++ client/app/scripts/utils/zoom-utils.js
@@ -1,5 +1,5 @@
 
 // See https://github.com/d3/d3-zoom/blob/807f02c7a5fe496fbd08cc3417b62905a8ce95fa/src/zoom.js
 export function defaultWheelDelta(ev) {
-  return ev.deltaY * (ev.deltaMode ? 120 : 1) * 0.002;
+  return ev.deltaY * (ev.deltaMode ? 15 : 1) * 0.0035;
 }
