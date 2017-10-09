b99047363607a09e25c567a54b185e1655f9f32d
Simon Howe <footless@gmail.com>
2017-10-09 12:12:51 +0200

    Use a context sensitive route for the popped out terminal.html
    - Service-ui has learned a new route which corresponds to the existing
      scope terminal.html route. Both standalone and service-ui scope can now
      respond to terminal.html:
      - If using standalone scope path/to/scope/ -> path/to/scope/terminal.html will be used.
      - If using service-ui app/instanceid -> app/instanceId/terminal.html

--- client/app/scripts/index.js
+++ client/app/scripts/index.js
@@ -1,3 +1,4 @@
 exports.reducer = require('./reducers/root').default;
 exports.Scope = require('./components/app').default;
 exports.actions = require('./actions/app-actions');
+exports.TerminalApp = require('./components/terminal-app').default;
