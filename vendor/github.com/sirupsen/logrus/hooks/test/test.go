80f8db5fa16af17f33135f0aa1f1b18517707178
Marc CARRE <carre.marc@gmail.com>
2017-10-20 11:38:16 +0100

    Upgrade dependencies to sirupsen/logrus v1.0.3.
    Commands run:
    ```
    $ gvt delete github.com/Sirupsen/logrus
    $ gvt fetch -tag v1.0.3 github.com/sirupsen/logrus
    ```
    N.B.: following this commit, build fails with:
    ```
    report/marshal.go:14:2: cannot find package "github.com/Sirupsen/logrus" in any of:
    	/go/src/github.com/weaveworks/scope/vendor/github.com/Sirupsen/logrus (vendor tree)
    	/usr/local/go/src/github.com/Sirupsen/logrus (from $GOROOT)
    	/go/src/github.com/Sirupsen/logrus (from $GOPATH)
    ```
    Resolution in subsequent commit.

--- vendor/github.com/sirupsen/logrus/hooks/test/test.go
+++ vendor/github.com/sirupsen/logrus/hooks/test/test.go
@@ -1,4 +1,7 @@
-package null
+// The Test package is used for testing logrus. It is here for backwards
+// compatibility from when logrus' organization was upper-case. Please use
+// lower-case logrus and the `null` package instead of this one.
+package test
 
 import (
 	"io/ioutil"
