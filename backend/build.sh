2ddcd9c067b4ec9c756d3a6f9c8faf86ee3ddba5
Bryan Boreham <bjboreham@gmail.com>
2021-04-04 11:37:28 +0100

    Add quotes required by shellcheck

--- backend/build.sh
+++ backend/build.sh
@@ -7,4 +7,4 @@ SCOPE_SRC=$GOPATH/src/github.com/weaveworks/scope
 # Mount the scope repo:
 #  -v $(pwd):/go/src/github.com/weaveworks/scope
 
-make -C $SCOPE_SRC BUILD_IN_CONTAINER=false $*
+make -C "$SCOPE_SRC" BUILD_IN_CONTAINER=false "$@"
